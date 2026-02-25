3dd2ada9644fce2d77f01185ce2dd3e1f4704e57
August Johnson <gurojohnson@gmail.com>
2026-02-24 20:34:39 -0500

    Set event listeners for delete and submit buttons Delete button now removes its own card and matching array object Submit button takes form input, updates array, creates new card, and resets form

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myLibrary = [];
 
+// constructor function
 function book(title, author, pages, read, rating, notes) {
     if (!new.target) {
         throw Error('You must use the "new" operator to call the constructor');
@@ -14,19 +15,18 @@ function book(title, author, pages, read, rating, notes) {
     this.notes = notes;
     }
 
-
+// tie to submit button
+// adds new book to array
 function addBookToLibrary() {
     //pull user input over
-    const title = document.getElementById('title');
-    const author = document.getElementById('author');
-    const pages = document.getElementById('pages');
-    const read = document.getElementById('read');
-    const rating = document.getElementById('bookRating');
-    const notes = document.getElementById('notes');
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const read = document.getElementById('read').value;
+    const rating = document.getElementById('bookRating').value;
+    const notes = document.getElementById('notes').value;
     // plug into constructor and push to array
-    book.push(new book(title, author, pages, read, rating, notes));
-
-
+    myLibrary.push(new book(title, author, pages, read, rating, notes));
 }
 
 myLibrary.push(new book('the hobbit', 'jrr tolkein', 500, 'yes', 5, 'no notes'));
@@ -38,20 +38,26 @@ console.log(myLibrary)
 // time to make the card for the book display
 const cardstack = document.createElement('div');
 cardstack.classList.add('cardstack');
+
 //grab container for cardstack to go to
-const container = document.getElementById('cardstack');
+const container = document.getElementById('container');
 container.appendChild(cardstack);
 
 // append children to create card.... in a function?
+//create card needs to be tied to submit button after constructor
 function createCard() {
+    cardstack.textContent = '';
     for (item of myLibrary) {
         // create card
         const card = document.createElement('div');
         card.classList.add('card');
+
+        const uuid = document.createElement('div');
+        uuid.classList.add('uuid');
         
         const deleteBtn = document.createElement('button');
-        deleteBtn.id = 'deleteBtn';
-
+        deleteBtn.classList.add('deleteBtn');
+        deleteBtn.id = 'deleteBtn'
 
         const title = document.createElement('p');
         title.classList.add('title');
@@ -77,10 +83,11 @@ function createCard() {
         const notes = document.createElement('p');
         notes.classList.add('notes');
         // append children-- append accepts multiple appendChild does not
-        card.append(deleteBtn, title, author, pages, read, rating, notes);
+        card.append(uuid, deleteBtn, title, author, pages, read, rating, notes);
         read.append(readIt, yesNo)
   
         //update text fields of card with array values
+        uuid.textContent = item.id;
         title.textContent = item.title;
         author.textContent = `Author: ${item.author}`;
         pages.textContent = `Length: ${item.pages} pages`;
@@ -90,20 +97,51 @@ function createCard() {
         notes.textContent = `${item.notes}`;
         //append card to cardstack
         cardstack.appendChild(card);
-        console.log(pages.textContent);
 };
 };
 
 createCard();
 
-// i have a submit button. i need a button to add new book which prompts form. i need a button that deletes a book
-
 // add event listeners to buttons to do the things
 
-const newBook = document.createElement('button');
-newBook.id = 'newBookBtn';
+
+// add new book button presents hidden form to fill out
+function showForm() {
+    document.getElementById('myForm').style.display = 'block';
+}
+
+const newBookBtn = document.getElementById('newBook');
+
+newBookBtn.addEventListener('click', showForm);
 
 
-// hey dummy, i made the buttons for new book and delete book. delete books got moved onto the card creation so that it is above the title and whatnot.
+// make the buttons do their jobs
 
-// i need to format the cards to look nice and then make the butons do their jobs
+const deleteBtn = document.getElementById('deleteBtn');
+
+
+
+
+
+// set all delete buttons to delete from array and card they're in
+cardstack.addEventListener('click', (event) => {
+    if (event.target.classList.contains('deleteBtn')) {
+// find book with matching id of card that is being deleted
+    const bookId = event.target.closest('.uuid');
+    const bookToRemove = myLibrary.findIndex(item => item.id === bookId);
+// remove it and for some reason needed to drop one off index?
+    myLibrary.splice((bookToRemove - 1), 1);
+
+    const deletedBook = event.target.closest('.card');
+    deletedBook.remove();}
+});
+
+
+// need submit button to make new book
+const form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    addBookToLibrary();
+    createCard();
+    form.reset();
+})
